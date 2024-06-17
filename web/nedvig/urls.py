a515dd0ac153b668ba8de7e14ff1d3c0e73d070c
Roman <you@example.com>
2024-06-17 22:27:50 +0300

    filter

--- web/nedvig/urls.py
+++ web/nedvig/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, Catalog, nedvig_page
+from .views import home, Catalog, NedvigDetailViews
 
 urlpatterns = [
     path('', home, name='home'),
     path('catalog/', Catalog.as_view(), name="catalog"),
-    path('catalog/nedvig_page/<int:pk>', nedvig_page, name="nedvig_page"),
+    path('catalog/nedvig_page/<int:pk>/', NedvigDetailViews.as_view(), name="nedvig_page"),
 ]
--- web/nedvig/views.py
+++ web/nedvig/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Nedvigimost
+from .models import Nedvigimost, PhotoNedvig, District, TypeNedvig, TypeUslugi, Street
 from django.views.generic.base import View
+from django.db.models import Q
 # Create your views here.
 
 def home(request):
@@ -9,9 +10,47 @@ def home(request):
 class Catalog(View):
     def get(self, request):
         nedvig_list = Nedvigimost.objects.all()
-        context = {'nedvig_list': nedvig_list}
+        type_nedvig = TypeNedvig.objects.all()
+        type_uslugi = TypeUslugi.objects.all()
+        districts    = District.objects.all()
+        streets      = Street.objects.all()
+        context = {'nedvig_list': nedvig_list,
+                   'type_nedvig': type_nedvig,
+                   'type_uslugi': type_uslugi,
+                   'districts': districts,
+                   'streets': streets}
+        return render(request, 'nedvig/catalog.html', context)
+    
+    def post(self, request):
 
+        print(request.POST)
+
+
+        type_uslug = request.POST.get('type_uslugi')
+        type_nedvigg = request.POST.get('type_nedvig')
+        districts = request.POST.get('district')
+        streets = request.POST.get('street')    
+        prices_start = request.POST.get('Price_start')
+        prices_end = request.POST.get('Price_end')
+
+        if prices_start == '':
+            prices_start = 0
+
+        if prices_end == '':
+            prices_end = 99999999    
+
+        nedvig_list = Nedvigimost.objects.filter(Q(type_uslugi = type_uslug) & Q(type_nedvig = type_nedvigg) & Q(street = streets) & Q(district = districts) & Q(price__range=(prices_start,prices_end)))   
+
+        context = {'nedvig_list': nedvig_list}
         return render(request, 'nedvig/catalog.html', context)
 
-def nedvig_page(request):
-    return render(request, 'nedvig/nedvig_page.html')
+    # def objects_filter(request):
+        
+
+class NedvigDetailViews(View):
+
+    def get(self, request, pk):
+        nedvig = Nedvigimost.objects.get(id=pk)
+        photo_nedvig = PhotoNedvig.objects.filter(kod_object = pk)
+        print(photo_nedvig, pk)
+        return render(request, 'nedvig/nedvig_page.html', {'nedvig': nedvig, 'photo_nedvig': photo_nedvig})
